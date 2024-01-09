0f1ffa3dcfcdf755f1c263a728ee135d8d75de30
Andrey <andreylitvinenko20102010@gmail.com>
2024-01-09 10:47:53 +0200

    add dependencies into requirements.txt file

--- parser.py
+++ parser.py
@@ -2,6 +2,11 @@ import json
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 HEADERS = {
