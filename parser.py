9c29c0f250d0c58361c4f9bd829582d70e79de6b
Andrey <andreylitvinenko20102010@gmail.com>
2024-01-09 10:44:08 +0200

    add function for first page parsing

--- parser.py
+++ parser.py
@@ -12,6 +12,30 @@ FIRST_PAGE_URL = "https://realtylink.org/en/properties~for-rent?uc=2"
 APARTMENTS_DATA = []
 
 
+def parse_main_page(url, max_pages=3):
+    page_count = 0
+    while page_count < max_pages:
+        response = requests.get(url, headers=HEADERS)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            apartments = soup.find_all("div", class_="property-thumbnail-item thumbnailItem col-12 col-sm-6 col-md-4 col-lg-3")
+            for apartment in apartments:
+                link = BASE_URL + str(apartment.find("a", class_="property-thumbnail-summary-link")).split()[2][7:-2]
+                parse_details_page(link)
+                if len(APARTMENTS_DATA) == 60:
+                    break
+
+            wait = WebDriverWait(driver, 10)
+            next_page_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li.next a")))
+            next_page_button.click()
+
+            page_count += 1
+        else:
+            print(f"Request error: {response.status_code}")
+
+
 def main():
     driver = webdriver.Chrome()
     driver.get("https://realtylink.org/en/properties~for-rent?uc=2")
