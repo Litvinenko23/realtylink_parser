7c4bf456dcde68a6c6d733f9b5e2a0f9041ff3b9
Andrey <andreylitvinenko20102010@gmail.com>
2024-01-09 11:15:04 +0200

    fixed code via flake & black

--- parser.py
+++ parser.py
@@ -11,7 +11,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/58.0.3029.110 Safari/537.3"
+}
 BASE_URL = "https://realtylink.org/"
 FIRST_PAGE_URL = "https://realtylink.org/en/properties~for-rent?uc=2"
 
@@ -26,15 +29,28 @@ def parse_main_page(url, driver, max_pages=3):
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "html.parser")
 
-            apartments = soup.find_all("div", class_="property-thumbnail-item thumbnailItem col-12 col-sm-6 col-md-4 col-lg-3")
+            apartments = soup.find_all(
+                "div",
+                class_="property-thumbnail-item thumbnailItem "
+                       "col-12 col-sm-6 col-md-4 col-lg-3",
+            )
             for apartment in apartments:
-                link = BASE_URL + str(apartment.find("a", class_="property-thumbnail-summary-link")).split()[2][7:-2]
+                link = (
+                    BASE_URL
+                    + str(
+                        apartment.find(
+                            "a", class_="property-thumbnail-summary-link"
+                        )
+                    ).split()[2][7:-2]
+                )
                 parse_details_page(link)
                 if len(APARTMENTS_DATA) == 60:
                     break
 
             wait = WebDriverWait(driver, 10)
-            next_page_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li.next a")))
+            next_page_button = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "li.next a"))
+            )
             next_page_button.click()
 
             page_count += 1
@@ -65,7 +81,7 @@ def parse_details_page(url):
             "images": images,
             "price": price,
             "rooms_quantity": rooms_quantity,
-            "floor_area": floor_area
+            "floor_area": floor_area,
         }
 
         APARTMENTS_DATA.append(apartment_data)
@@ -92,7 +108,9 @@ def extract_description(soup):
 
 
 def extract_images(soup):
-    script_tag = soup.find("script", string=re.compile("window.MosaicPhotoUrls"))
+    script_tag = soup.find(
+        "script", string=re.compile("window.MosaicPhotoUrls")
+    )
     script_content = script_tag.string
     photo_urls = re.findall(r'"(https://[^"]+)"', script_content)
     return photo_urls
